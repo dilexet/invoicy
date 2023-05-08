22994d6d6f0f527412c06b3cd8f31b41e9c603ea
Pavarha Maksim <dilexet@gmail.com>
2023-05-08 13:10:56 +0300

    Added data-source initializing to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { dataSource } from './database/data-source';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  dataSource
+    .initialize()
+    .then(async () => {
+      const app = await NestFactory.create(AppModule);
+      await app.listen(3000);
+    })
+    .catch((error) => console.log(error));
 }
+
 bootstrap();
