dedc0a4cfce632940f7a7a3d06726e516de33d55
Pavarha Maksim <dilexet@gmail.com>
2023-05-08 13:29:12 +0300

    Changed server startup settings using .env variables

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { dataSource } from './database/data-source';
+import * as process from 'process';
 
 async function bootstrap() {
   dataSource
     .initialize()
     .then(async () => {
       const app = await NestFactory.create(AppModule);
-      await app.listen(3000);
+      console.log();
+      await app.listen(process.env.PORT, process.env.HOST, () => {
+        console.log(
+          `Server started on http://${process.env.HOST}:${process.env.PORT}/`,
+        );
+      });
     })
     .catch((error) => console.log(error));
 }
