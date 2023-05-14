926750d2791970e405f37170db462c2f72451581
Pavarha Maksim <dilexet@gmail.com>
2023-05-14 13:46:29 +0300

    Changed invoice entities

--- src/database/entity/invoice.entity.ts
+++ src/database/entity/invoice.entity.ts
@@ -1,8 +1,21 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { CompletedWorkEntity } from './completed-work.entity';
+import { AutoMap } from '@automapper/classes';
 
 @Entity()
 export class InvoiceEntity extends BaseEntity {
-  @Column()
+  @Column({ unique: true, generated: 'increment', nullable: false })
+  @AutoMap()
+  invoiceNumber: number;
+
+  @Column({ nullable: false })
+  @AutoMap()
   email: string;
+
+  @OneToMany(() => CompletedWorkEntity, (entity) => entity.invoice, {
+    cascade: true,
+    onDelete: 'SET NULL',
+  })
+  completedWorks: CompletedWorkEntity[];
 }
