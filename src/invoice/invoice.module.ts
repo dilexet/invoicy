46c64ac86fb408fd4fa077a02013e1aa65b17686
Pavarha Maksim <dilexet@gmail.com>
2023-05-14 13:49:08 +0300

    Added creating invoice

---
+++ src/invoice/invoice.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { InvoiceService } from './invoice.service';
+import { InvoiceController } from './invoice.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InvoiceEntity } from '../database/entity/invoice.entity';
+import { CompletedWorkEntity } from '../database/entity/completed-work.entity';
+import { InvoiceMapperProfile } from './mapper-profile/invoice.mapper-profile';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([InvoiceEntity, CompletedWorkEntity])],
+  controllers: [InvoiceController],
+  providers: [InvoiceService, InvoiceMapperProfile],
+})
+export class InvoiceModule {}
