be47f2f84b0a1b610c595f5939c8973b7a085795
Pavarha Maksim <dilexet@gmail.com>
2023-07-19 14:13:42 +0300

    Moved event name to .env file

--- src/modules/invoice/invoice.consumer.ts
+++ src/modules/invoice/invoice.consumer.ts
@@ -4,14 +4,22 @@ import { INVOICE_GENERATE_QUEUE_NAME } from '../../constants/queue.constants';
 import { eventManager } from '../../utils/event-manager';
 import { InvoiceFileViewModel } from './view-model/invoice-file.view-model';
 import { InvoiceGenerator } from './utils/invoice.generator';
+import { Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Processor(INVOICE_GENERATE_QUEUE_NAME)
 export class InvoiceConsumer {
-  constructor(private readonly invoiceGenerator: InvoiceGenerator) {}
+  constructor(
+    @Inject(ConfigService)
+    private readonly configService: ConfigService,
+    private readonly invoiceGenerator: InvoiceGenerator,
+  ) {}
 
   @Process()
   async process(job: Job<InvoiceFileViewModel>): Promise<void> {
     await this.invoiceGenerator.generateInvoice(job.data);
-    eventManager.emit('invoiceGenerated');
+    eventManager.emit(
+      this.configService.get<string>('INVOICE_GENERATION_EVENT_NAME'),
+    );
   }
 }
--- src/modules/mail/mail.consumer.ts
+++ src/modules/mail/mail.consumer.ts
@@ -4,17 +4,26 @@ import { MAIL_SENDER_QUEUE_NAME } from '../../constants/queue.constants';
 import { MailService } from './mail.service';
 import { MailInfoDto } from './mail-info.dto';
 import { eventManager } from '../../utils/event-manager';
+import { Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Processor(MAIL_SENDER_QUEUE_NAME)
 export class MailConsumer {
-  constructor(private readonly mailService: MailService) {}
+  constructor(
+    @Inject(ConfigService)
+    private readonly configService: ConfigService,
+    private readonly mailService: MailService,
+  ) {}
 
   @Process()
   async process(job: Job<MailInfoDto>): Promise<boolean> {
     await new Promise<void>((resolve) => {
-      eventManager.once('invoiceGenerated', () => {
-        resolve();
-      });
+      eventManager.once(
+        this.configService.get<string>('INVOICE_GENERATION_EVENT_NAME'),
+        () => {
+          resolve();
+        },
+      );
     });
     return await this.mailService.sendMailAsync(job.data);
   }
