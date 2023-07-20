0e4654c326c8f5904049ec492a0d53510ef7a2c1
Pavarha Maksim <dilexet@gmail.com>
2023-07-20 14:27:37 +0300

    Added getting all companies

--- src/modules/company-management/company-management.service.ts
+++ src/modules/company-management/company-management.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectMapper } from '@automapper/nestjs';
 import { Mapper } from '@automapper/core';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 import { CompanyEntity } from '../../database/entity/company.entity';
 import { CompanyViewModel } from './view-model/company.view-model';
 import { CreateCompanyInfoDto } from './dto/create-company-info.dto';
@@ -43,5 +43,19 @@ export class CompanyManagementService {
       CompanyEntity,
       CompanyViewModel,
     );
+  }
+
+  async getAllAsync(company?: string): Promise<CompanyViewModel[]> {
+    const companies = !company
+      ? await this.companyEntityRepository.find()
+      : await this.companyEntityRepository.find({
+          where: [{ name: ILike(`%${company}%`) }],
+        });
+
+    if (companies.length <= 0) {
+      return [];
+    }
+
+    return this.mapper.mapArray(companies, CompanyEntity, CompanyViewModel);
   }
 }
